b4a588c9518a906a073d5c01720c1becd93d6b91
Apirak <kittypunkz.blog@gmail.com>
2023-10-08 23:46:17 +0700

    Add DogDoor Attribute and Function on DogDoor.cs

---
+++ DogDoor.cs
@@ -0,0 +1,15 @@
+public class DogDoor {
+    //Door Attribute
+    public string DoorSize;
+    public string DoorMaterial;
+    public int DoorHeight;
+
+    //Function
+    public string DoorOpen() {
+        return "Open!";
+    }
+
+    public string DoorClose() {
+        return "Close!";
+    }
+}
