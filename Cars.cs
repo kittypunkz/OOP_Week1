7411503fb7f657357f6a4307036e0a31b249318e
Apirak <kittypunkz.blog@gmail.com>
2023-10-08 23:17:02 +0700

    Add Car Attribute and Function on Cars.cs

---
+++ Cars.cs
@@ -0,0 +1,17 @@
+public class Car {
+    //Car Attribute
+    public string brand;
+    public string model;
+    public int numberOfDoors;
+    public int numberOfWheels;
+    public int color;
+
+    //Car Function
+    public string MoveFoward() {
+        return "Move Forward"; 
+    }
+
+    public string MoveBack() {
+        return "Move Back";
+    }
+}
